da18c84524422e3f1d793495f6daa1522997b64c
meustar <meustar829@gmail.com>
2026-02-25 01:39:16 +0900

    news api로 데이터 받아오며, async, await 비동기 함수 사용하기. json 형태의 데이터 확인까지

--- news-times/main.js
+++ news-times/main.js
@@ -1,12 +1,18 @@
 const API_KEY = `1ca1f4d7b9d44fa8bff490ce56226976`;
-const getLatestNews = () => {
+let news = [];
+
+const getLatestNews = async () => {
   const url = new URL(
     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`,
   );
-  const response = fetch(url); // url을 이용해서 fetch로 데이터를 가져온다.
+  const response = await fetch(url); // url을 이용해서 fetch로 데이터를 가져온다.
+  const data = await response.json();
+  news = data.articles;
+  console.log("dddd", news);
 };
 
 getLatestNews();
-for (let i = 0; i < 20; i++) {
-  console.log("after", i);
-}
+
+// for (let i = 0; i < 20; i++) {
+//   console.log("after", i);
+// }
