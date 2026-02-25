315208a8bb9f3be1e3b157036a5c91ac930c013e
meustar <meustar829@gmail.com>
2026-02-25 19:14:22 +0900

    기사 내용이 200자 이상일 경우 "..."로 대체, 내용이 없을 경우 "내용 없음"으로 대체, 대표이미지가 없을 경우 대체이미지 추가, 출처 없을 경우 "no source" 표기

--- news-times/main.js
+++ news-times/main.js
@@ -36,23 +36,38 @@ function searchButton() {
 
 const render = () => {
   const newsHTML = newsList
-    .map(
-      (news) => `<div class="row news">
+    .map((news) => {
+      // description 내용이 없으면 "내용없음", 200자 넘으면 이후 "..."으로 표시
+      const description = news.description
+        ? news.description.length > 200
+          ? news.description.substring(0, 200) + "..."
+          : news.description
+        : "내용 없음";
+      // news.urlToImage에 이미지가 없거나 null일 경우
+      const imageUrl = news.urlToImage
+        ? news.urlToImage
+        : "https://cdn.vectorstock.com/i/500p/01/56/no-image-symbol-missing-available-icon-gallery-vector-44020156.jpg";
+
+      // news.source.name (출처)가 없으면 'no source'
+      const sourceName =
+        news.source && news.source.name ? news.source.name : "no source";
+
+      return `<div class="row news">
           <div class="col-lg-4">
             <img
               class="news-img-size"
-              src=${news.urlToImage}
+              src=${imageUrl}
             />
           </div>
           <div class="col-lg-8">
             <h2>${news.title}</h2>
-            <p>${news.description}</p>
-            <div>${news.source.name} * ${news.publishedAt}</div>
+            <p>${description}</p>
+            <div>${sourceName} * ${news.publishedAt}</div>
           </div>
-        </div>`,
-    )
+        </div>`;
+    })
     .join("");
-  console.log("html", newsHTML);
+  // console.log("html", newsHTML);
 
   document.getElementById("news-board").innerHTML = newsHTML;
 };
